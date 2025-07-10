4089aaf21c107f973e8cbdee512fc2eaebb9b658
Shaik Khaja Mohid <mohidshaik2001@gmail.com>
2025-07-11 04:53:59 +0530

    sdc

--- 09themechanger/src/App.jsx
+++ 09themechanger/src/App.jsx
@@ -6,37 +6,34 @@ import Card from "./componenets/Card";
 import { ThemeProvider } from "./context/theme";
 
 function App() {
+  const [themeMode, setThemeMode] = useState("light");
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
 
-  const [themeMode,setThemeMode]  = useState("light")
-  const darkTheme = () =>{
-    setThemeMode("dark")
-  }
-  const lightTheme = () =>{
-    setThemeMode("light")
-  }
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
 
-  useEffect(()=>{
-    document.querySelector('html').classList.remove("light","dark")
-    document.querySelector('html').classList.add(themeMode)
-
-  },[themeMode])
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
 
   return (
-    <ThemeProvider value={{themeMode,darkTheme,lightTheme}}>
-
+    <ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
       <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-            <ThemeBtn/>
+            <ThemeBtn />
           </div>
 
           <div className="w-full max-w-sm mx-auto">
-            <Card/>
+            <Card />
           </div>
         </div>
       </div>
     </ThemeProvider>
-    
   );
 }
 
